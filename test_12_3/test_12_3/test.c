69cbe768e43db241db040618a4c1acea669790c2
wang-gang-victory <2541456467@qq.com>
2025-12-03 21:10:04 +0800

    上课代码

---
+++ test_12_3/test_12_3/test.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//char
+//[signed] char
+//[unsigned] char
+
+
+
+//#include <stdio.h>
+//#include <stdbool.h>
+//
+//int main()
+//{
+//	bool a = true;
+//	if (a)
+//		printf("like c");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 100;
+//	printf("%zd\n", sizeof a);
+//	printf("%zd\n", sizeof(a));
+//	printf("%zd\n", sizeof(int));
+//	printf("%zd\n", sizeof(3 + 3.5));
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%zd\n", sizeof(char));
+//	printf("%zd\n", sizeof(_Bool));
+//	printf("%zd\n", sizeof(short));
+//	printf("%zd\n", sizeof(int));
+//	printf("%zd\n", sizeof(long));
+//	printf("%zd\n", sizeof(long long));
+//	printf("%zd\n", sizeof(float));
+//	printf("%zd\n", sizeof(double));
+//	printf("%zd\n", sizeof(long double));
+//
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	short a = 2;
+//	int b = 4;
+//	printf("%zd\n", sizeof(a = b + 1));
+//	printf("%d\n", a);
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int b = 45;
+//int main()
+//{
+//	int a = 199;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+#include <stdio.h>
+int a = 100;
+int main()
+{
+	int a = 45;
+	printf("%d\n", a);
+	return 0;
+}
