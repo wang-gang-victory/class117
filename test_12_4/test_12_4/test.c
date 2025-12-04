bc738cc42d0eaf98e8f15b517be17af9cd30d180
wang-gang-victory <2541456467@qq.com>
2025-12-04 21:51:46 +0800

    课堂代码

---
+++ test_12_4/test_12_4/test.c
@@ -0,0 +1,202 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x = 4 + 22;
+//	int y = 61 - 23;
+//	printf("%d\n", x);
+//	printf("%d\n", y);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int mun = 5;
+//	printf("%d\n", mun*mun );
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	float x = 6 / 4;
+//	int y = 6 / 4;
+//	printf("%f\n", x);
+//	printf("%d\n", y);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	float x = 6.0 / 4;
+//	printf("%f\n", x);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int score = 5;
+//	score = (score / 20) * 100;
+//	printf("%d\n", score);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int score = 5;
+//	score = (score / 20.0) * 100;
+//	printf("%d\n", score);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int x = 6 % 4;
+//	printf("%d\n", x);
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n", 11 % -5);
+//	printf("%d\n", -11 % 5);
+//	printf("%d\n", -11 % -5);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("hello\nworld\n");
+//	printf("hello\n");
+//	printf("world\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("there are %d apples\n",3);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%s will come tonight\n", "zhangsan");
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%s says it is %d o'clock\n", "zhangsan", 21);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%5d\n", 123);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%-5dxx\n", 123);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%12f\n", 123.45);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%+d\n", 12);
+//	printf("%+d\n", -12);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("number is %.2f\n", 0.5);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%6.2f", 0.5);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%*.*f\n", 6,2,0.5);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%.5s\n", "hello warld");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int score = 0;
+//	printf("请输入成绩：");
+//	scanf("%d", &score);
+//	printf("成绩是：%d\n", score);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int x = 0;
+//	float y = 0;
+//	scanf("%d", &x);
+//	
+//	scanf("%f", &y);
+//	printf("%d\n", x);
+//	printf("%f\n", y);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int x = 0;
+//	float y = 0;
+//	scanf("%d%f", &x, &y);
+//	printf("%d\n%f\n", x, y);
+//	return 0;
+//}
+
