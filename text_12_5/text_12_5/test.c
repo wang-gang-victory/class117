6fa31f74596a4d5b46829d397d9d98485e263989
wang-gang-victory <2541456467@qq.com>
2025-12-05 23:07:28 +0800

    上课代码

---
+++ text_12_5/text_12_5/test.c
@@ -0,0 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//输⼊⼀个整数，判断是否为奇数
+//#include <stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2 == 1)
+//		printf("num是奇数");
+//	else
+//		printf("num是偶数");
+//	return 0;
+//}
+
+//：输⼊⼀个年龄， >= 18岁就输出：成年，否则就输出：未成年
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age >= 18)
+//		printf("成年");
+//	else
+//		printf("未成年");
+//	return 0;
+//}
+//
+//默认在
+//if
+//和
+//else
+//语句中默认都只控制⼀条语句，⽐如：
+
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age >= 18)
+//		printf("成年了\n");
+//	    printf("可以谈恋爱了\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age >= 18)
+//	{
+//		printf("成年了\n");
+//		printf("可以谈恋爱了\n");
+//	}
+//	else
+//	{
+//		printf("未成年\n");
+//		printf("不可以谈恋爱\n");
+//	}
+//	return 0;
+//}
+
+//⽐如：要求输⼊⼀个整数，判断输⼊的整数是0，还是正数或者负数。请看如下代码：
+
+//#include <stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num == 0)
+//		printf("%d是0\n", num);
+//	else if (num > 0)
+//		printf("%d是正数\n", num);
+//	else if (num < 0)
+//		printf("%d是负数\n", num);
+//	
+//	return 0;
+//}
+
