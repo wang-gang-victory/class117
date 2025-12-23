284b668bc6aa84f3596d69b024ec4d401a05b728
wang-gang-victory <2541456467@qq.com>
2025-12-23 23:08:32 +0800

    练习代码

---
+++ test_12_23_night/test_12_23_night/test.c
@@ -0,0 +1,108 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%p\n", &arr[i]);
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%d\n", sizeof(arr[0]));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", sz);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[3][5] = { 1,2,3,4,5, 2,3,4,5,6, 3,4,5,6,7 };
+//	printf("%d\n", arr[2][2]);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[3][5] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 5; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[3][5] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("&arr[%d][%d]=%d\n",i,j, &arr[i][j]);
+//		}
+//	}
+//	return 0;
+//}
+
