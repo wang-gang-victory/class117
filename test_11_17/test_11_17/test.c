954c2f516b6b04a098ce1d5a67b8ddc77d4e6a0c
wang-gang-victory <2541456467@qq.com>
2026-05-27 20:52:24 +0800

    上课练习代码

---
+++ test_11_17/test_11_17/test.c
@@ -0,0 +1,137 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+////古老的写法不推荐
+//void main()
+//{
+//
+//}
+
+//int main(void)
+//{
+//
+//	return 0;
+//}
+
+//main函数是有参数的
+//int main(int argc, char* argv[])
+//{
+//
+//
+//	return 0;
+//}
+
+//推荐
+//int main()
+//{
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("hello C\n");
+//	printf("hello C\n");
+//	printf("hello C\n");
+//	printf("hello C\n");
+//	printf("hello C\n");
+//	printf("hello C\n");
+//	printf("hello C\n");
+//	printf("hello C\n");
+//	printf("hello C\n");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("hello C\n");
+//	printf("呵呵\n");
+//
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%d\n",15);
+//	printf("%c\n",'x');
+//	printf("%s\n","adfafa");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//	char arr1[] = "abcdef";
+//	char arr2[] = {'a','b','c','d','e','f','\0'};
+//
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("ab\nc\nd\n");
+//	return 0;
+//
+//}
+
+//三字母词
+//??)  ]
+//??(  [
+//??>  }
+//??<  {
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("(are you ok??)");
+//	//are you ok]
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%c\n", '\'');
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("%zd\n", sizeof("c:\\test\\test117\\add.c"));
+	printf("%c\n",'\'');
+	printf("%s\n","abc");
+	printf("%s\n","a");
+	printf("%s\n","\"");
+	printf("c:\\test\\test117\\add.c");
+	return 0;
+}
+
+
+
+
+
+
+
+
