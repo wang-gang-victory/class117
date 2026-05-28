12d24083e848b00ad72f38820708c59a80f1aaf8
wang-gang-victory <2541456467@qq.com>
2026-05-28 23:28:09 +0800

    上课代码

---
+++ test_11_22/test_11_22/test.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("%d\n", 3 + 5);
+//	printf("%d\n", a + b);
+//	printf("%d\n", a - b);
+//	printf("%d\n", 3 - 5);
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%d\n", 8 / 2);
+//	printf("%d\n", 8 / 3);
+//	printf("%d\n", 6 / 4);
+//	printf("%f\n", 6 / 4.0);
+//	printf("%f\n", 6.0 / 4);
+//	printf("%f\n", 6.0 / 4.0);
+//	return 0;
+//}
+
+//%f用来打印小数，这种打印默认是小数点后六位
+//除号两端只要有一端是小数，就属于小数除法
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int score = 5;
+//	score = (score / 20.0) * 100;
+//	printf("%d\n", score);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%d\n", 10 % 3);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	//int a = 0;//初始化
+//	//a = 10;//赋值
+//
+//	int a = 3;
+//	int b = 5;
+//	int c = 0;
+//	c = b = a + 3;
+//	printf("%d\n", c);
+//	printf("%d\n", b);
+//	c = (b = a + 3);
+//	printf("%d\n", c);
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 3;
+//	a = a + 3;
+//	a += 3;
+//	a = a - 5;
+//	a -= 5;
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
