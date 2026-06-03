e5aab60672fa8dc9ebbef1eb2006aa5c31949855
wang-gang-victory <2541456467@qq.com>
2026-06-03 21:08:25 +0800

    上课代码补充

--- test_11_22/test_11_22/test.c
+++ test_11_22/test_11_22/test.c
@@ -1,5 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+
+#include <stdio.h>
+
 //#include <stdio.h>
 //
 //int main()
@@ -79,6 +82,250 @@
 //	a -= 5;
 //	printf("%d\n", a);
 //
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 5;
+//	a = a + 1;
+//	a += 1;
+//	a++;
+//	++a;
+//	printf("%d\n", ++a);
+//	printf("%d\n", a++);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 5;
+//	int b = a++;
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 5;
+//	int b = --a;
+//	printf("%d\n", b);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	printf("%d\n", (a++) + (++a) + (a++)); //错误的代码，编译器计算结果是6
+//	//        a      1       3       3
+//	//     表达式：   1       4        7
+//
+//	return 0;
+//
+//
+//}
+
+//int main()
+//{
+//	int a = +10;
+//	printf("%d\n", a);
+//	printf("%d\n", +a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = -10;
+//	printf("%d\n", a);
+//	printf("%d\n", -a);
+//	
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int a = (int)3.14;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("hello\nworld\n");
+//	printf("hello\n");
+//	printf("world\n");
+//	return 0;
+//}
+
+//%d - 有符号的整数占位符
+
+//int main()
+//{
+//	//printf("%d\n", 555);
+//	printf("there are 3 apples\n");
+//	printf("there are %d apples\n",4);
+//	printf("there are %d apples\n",500);
+//	printf("there are %d apples\n",202);
+//	return 0;
+//}
+
+//%s - 字符串的占位符
+
+//int main()
+//{
+//	printf("zhangsan will come tonight\n");
+//	printf("%s will come tonight\n", "zhangsan");
+//	printf("%s will come tonight\n","李四");
+//	printf("%s will come tonight\n","wangwu");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("zhangsan says it is 8 o'clock\n");
+//	printf("%s says it is %d o'clock\n", "zhangsan", 8);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("%d\n", 6/4);
+//	short int a = 2;
+//	printf("%hd\n", a);
+//	unsigned short int m = 5;
+//	printf("%hu\n", m);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 15;
+//	printf("%%d\n", a);
+//	printf("%x\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%-5d%d\n", 123,123);
+//	printf("%6d\n", 153);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%5d\n", 123456);
+//	printf("%05d\n", 123);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%5lf\n", 3.4);
+//	printf("%lf\n", 3.4);
+//	printf("%12lf\n", 3.4);
+//	printf("%12.8lf\n", 3.4);
+//	printf("%-12.8lf\n", 3.4);
+//	printf("%012.8lf\n", 3.4);
+//	printf("%012lf\n", 3.4);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%+d\n", 15);
+//	printf("%+d\n", -15);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%-d\n", 15);
+//	printf("%-d\n", -15);//前面夹负号没什么用
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	printf("%f\n",123.456);
+//	printf("%.2f\n",123.456);
+//	printf("%.3f\n",123.456);
+//	printf("%.1f\n",123.456);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%8.2f\n", 123.456);
+//	printf("%*.*f\n", 10, 3, 123.456);
+//	return 0;
+//}
+
+//输出部分字符串%.5s
+
+//int main()
+//{
+//	printf("%s\n", "hello world");
+//	printf("%.5s\n", "hello world");
+//	printf("%50s\n", "hello world");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int score = 0;
+//	scanf("%d", &score);
+//
+//	printf("成绩是：%d\n", score);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	double b = 0;
+////	char c = 'g';
+//	scanf("%d", &a);
+//	scanf("%lf", &b);
+//	scanf("%c", &c);
+//	printf("%d\n", a);
+//	printf("%lf\n", b);
+//	//printf("%c\n", c);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	float c = 0;
+//	int r = scanf("%d %d %f", &a, &b, &c);
+//	printf("%d\n%d\n%f\n", a, b, c);
+//	printf("%d\n", r);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[5] = { 0 };
+//	scanf("%s", arr);
+//	printf("%s", arr);
 //	return 0;
 //}
 
